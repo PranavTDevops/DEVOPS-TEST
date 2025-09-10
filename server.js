a67f776bf5603f278ac510b01f7f6c0a8263244b
PranavTDevops <163020616+PranavTDevops@users.noreply.github.com>
2025-09-11 01:46:39 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const http = require('http');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; // choose 3000 for now
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end('<h1>Hello from AWS EC2 Web Server!</h1>');
 });
 
-const port = process.env.PORT || 80;
-
 server.listen(port, '0.0.0.0', () => {
   console.log(`Server running at http://0.0.0.0:${port}`);
 });
-
-
-
